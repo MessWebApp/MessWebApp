58ff6bd0b03029814b756ea0ecc0fee89029981b
Aditya2462001 <aditya2462001@gmail.com>
2021-12-02 13:59:40 +0530

    connecting the files

--- messApp/urls.py
+++ messApp/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('',views.Home,name='Home'),
+
+    #  ================== customer login ==================
+    path('customer-login/',views.CustomerLoginView,name='customer-login'),
+    path('customer-register/',views.CustomerRegister,name='customer-register'),
+
+    #  ================== customer login ==================
+    path('supplier-login/',views.SupplierLoginView,name='supplier-login'),
+    path('supplier-register/',views.SupplierRegister,name='supplier-register'),
 ]
--- messApp/views.py
+++ messApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout 
 from django.contrib import messages
-from django.contrib.auth.models import User
-from .models import Customer,Supplier,MessDetails,MessBooking,MessReview
+# from django.contrib.auth.models import User
+from .models import Customer,Supplier,MessDetails,MessBooking,MessReview,User
 import random
 from django.core.mail import EmailMessage , send_mail
 
@@ -36,32 +36,7 @@ def SupplierLoginView(request):
             return redirect('/supplier-login')
 
     context = {}
-    return render(request,'supplier-login.html',context)
-
-# ======================== customer login =========================
-def CustomerLoginView(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = authenticate(username = email,password = password)
-
-        if user is not None:
-            if user.is_active and user.is_customer:
-                customer = Customer.objects.get(user = user)
-                if customer.email_verify:
-                    login(request,user)
-                    redirect('')
-                else:
-                    messages.warning(request,'your account not verified please verified then login.')
-                    redirect('/email-verification')
-        else:
-            messages.warning(request,'Please check the credential')
-            return redirect('/customer-login')
-
-    context = {}
-    return render(request,'customer-login.html',context)
-
+    return render(request,'supplier/supplier-login.html',context)
 
 # =================== register supplier =============================
 
@@ -70,7 +45,7 @@ def SupplierRegister(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         number = request.POST.get('number')
-        vpassword = request.POSt.get('vpassword')
+        vpassword = request.POST.get('vpassword')
         password = request.POST.get('password')
 
         if User.objects.filter(email = email,number=number).exists():
@@ -104,7 +79,7 @@ def SupplierRegister(request):
         messages.success(request,'Verfiy your Profile.Otp sended to your email.')
         return redirect('/supplier-sendOTP')
 
-    return render(request,'supplier-register.html',{})
+    return render(request,'supplier/supplier-register.html',{})
 
 
 def SupplierSendOPT(request):
@@ -127,15 +102,42 @@ def SupplierSendOPT(request):
 
 # =================== register supplier =============================
 
+
+
+# ======================== customer login =========================
+def CustomerLoginView(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username = email,password = password)
+
+        if user is not None:
+            if user.is_active and user.is_customer:
+                customer = Customer.objects.get(user = user)
+                if customer.email_verify:
+                    login(request,user)
+                    redirect('')
+                else:
+                    messages.warning(request,'your account not verified please verified then login.')
+                    redirect('/email-verification')
+        else:
+            messages.warning(request,'Please check the credential')
+            return redirect('/customer-login')
+
+    context = {}
+    return render(request,'customer/customer-login.html',context)
+
+
 def CustomerRegister(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         number = request.POST.get('number')
-        vpassword = request.POSt.get('vpassword')
+        vpassword = request.POST.get('vpassword')
         password = request.POST.get('password')
 
-        if User.objects.filter(email = email,number=number).exists():
+        if User.objects.filter(username = email,number=number).exists():
             messages.warning(request,'user is Exits please login in site.')
             return redirect('/customer-login')
         
@@ -153,7 +155,7 @@ def CustomerRegister(request):
 
         request.session['email'] = email
 
-        customer = Customer(name = name,email = email,number = number,otp = otp)
+        customer = Customer(user = user, name = name,email = email,number = number,otp = otp)
         customer.save()
 
         mail = EmailMessage(
@@ -166,7 +168,7 @@ def CustomerRegister(request):
         messages.success(request,'Verfiy your Profile.Otp sended to your email.')
         return redirect('/customer-sendOTP')
 
-    return render(request,'customer-register.html',{})
+    return render(request,'customer/customer-register.html',{})
 
 
 def CustomerSendOPT(request):
