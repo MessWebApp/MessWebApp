1d69848b193ede8c32335d5074dd983407e9c53a
Aditya2462001 <aditya2462001@gmail.com>
2021-12-11 21:50:48 +0530

    work on backend

--- messApp/form.py
+++ messApp/form.py
@@ -2,10 +2,11 @@ from django import forms
 from django.db import models
 from django.forms import fields
 
-from .models import ContactMe
+from .models import ContactMe,MessDetails
 
 
 class ContactForm(forms.Form):
     class Meta:
         models = ContactMe
         fields = ('name','email','number','message')
+
